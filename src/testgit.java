8f53a4a412e04b6baa97ef288cf07b8e758106c7
邹丽鹏 <960990381>
2023-05-10 21:40:22 +0800

    第二次修改

--- src/testgit.java
+++ src/testgit.java
@@ -1,5 +1,5 @@
 public class testgit {
     public static void main(String[] args) {
-        System.out.println("hello git! ---add content");
+        System.out.println("hello git! ---add content22222");
     }
 }
